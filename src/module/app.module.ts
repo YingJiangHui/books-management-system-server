50aaa5e5659eeeb20cb48550af9594b2688eee4c
Jh <473380917@qq.com>
2021-07-17 18:22:18 +0800

    feat: 连接数据库

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from '../controller/app.controller';
 import { BookController } from '../controller/book.controller';
 import { AppService } from '../service/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController, BookController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}
