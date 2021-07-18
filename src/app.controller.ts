77d11d2fde852bb90d1fa1b4f36d20dda3c04491
Jh <473380917@qq.com>
2021-07-18 22:02:41 +0800

    refactor: 代码抽离
    抽离出本地权限守卫

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
   
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     return req.user;
   }
+
 }
