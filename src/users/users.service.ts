56cb00f91bbd47e2bc28ad5c123da4ff02559fb4
Jh <473380917@qq.com>
2021-07-19 23:08:34 +0800

    feat: typeorm实现关联查询
    查询用户信息与权限表关联

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
+import { Role } from '../roles/role.entity';
 
 
 @Injectable()
@@ -12,9 +13,12 @@ export class UsersService {
   find(): Promise<User[]> {
     return this.usersRepository.find();
   }
-  findOne(username:string):Promise<User|undefined>{
+  async findOne(username:string):Promise<User|undefined>{
     // select user2.id ,user2.username,user2.email,role.name from public.user as user2,role,user_roles_role  where user2.username='user' AND role.id = "roleId" AND user2.id = "userId";
-    return this.usersRepository.findOne({username});
+    return this.usersRepository.findOne({
+      where:{username},
+      relations:['roles']
+    })
   }
   async createOne(user: User): Promise<User> {
     await this.usersRepository.create(user)
