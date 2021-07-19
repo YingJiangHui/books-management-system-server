109d5d78679fb7b58baa21ab793532af2f64e271
Jh <473380917@qq.com>
2021-07-19 22:26:51 +0800

    feat: 权限守卫
    创建权限首位，前提是代码默认 user 对象拥有 roles 属性

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,16 @@ import { Connection } from 'typeorm';
 import { UsersModule } from './users/users.module';
 import { NationsModule } from './nations/nations.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './roles/roles.guard';
 
 @Module({
   imports: [TypeOrmModule.forRoot(),UsersModule,NationsModule,AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,  {
+    provide: APP_GUARD,
+    useClass: RolesGuard,
+  },],
 })
 export class AppModule {
   // 注入connection后在整个项目中都可是使用不需要再导入，使用方法在类的构造函数中传递即可
