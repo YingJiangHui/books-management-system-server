e656da2c3b04df910c8e0703cb47292bd155cd75
Jh <473380917@qq.com>
2021-07-20 18:27:01 +0800

    feat: 使用用户发送的cookie作为认证token
    1. 安装依赖cookie-parser
    2. 用户登录之后将生成的jwt存入cookie
    3.
    用户调用受保护的接口时，将token传给JwtStrategy类构造函数options.jwtFromRequest属性（token从请求上下文中获取）

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,7 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { Roles } from './roles/roles.decorator';
 import { Role } from './roles/role.enum';
 import { RolesGuard } from './roles/roles.guard';
+import {Response} from 'express'
 
 // 下面是两种 Passport 策略,护照本地策略和护照 jwt 策略
 @Controller()
@@ -13,8 +14,9 @@ export class AppController {
   
   @UseGuards(LocalAuthGuard) // 不受保护的本地守卫，LocalStrategy.validate -> AuthService.validateUser -> AppController.login -> authService.login
   @Post('auth/login')
-  async login(@Req() req) {
+  async login(@Req() req,@Res({ passthrough: true }) res:Response) {
     const token = await this.authService.login(req.user)
+    res.cookie('jwt', token)
     // Passport 将基于 validate() 方法的返回值构建一个user 对象，并将其作为属性附加到请求对象上。
     return token;
   }
@@ -22,7 +24,7 @@ export class AppController {
   @UseGuards(JwtAuthGuard,RolesGuard) // 测试代码，受jwt保护的jwt守卫
   @Roles(Role.User)
   @Get('profile')
-  getProfile(@Res() req) {
+  getProfile(@Req() req) {
     return req.user;
   }
 }
