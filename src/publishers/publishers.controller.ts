62897503bfbb66a69961cbb35d71dea3020b536f
Jh <473380917@qq.com>
2021-07-25 16:48:33 +0800

    feat: publisher 相关接口提供

--- src/publishers/publishers.controller.ts
+++ src/publishers/publishers.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { PublishersService } from './publishers.service';
 
+@UseGuards(JwtAuthGuard)
 @Controller('publishers')
-export class PublishersController {}
+export class PublishersController {
+  constructor(private readonly publishersService: PublishersService) {}
+  
+  @Get()
+  getCategories() {
+    return this.publishersService.find()
+  }
+  
+  @Get(':id')
+  getCategory(@Param('id') id:number) {
+    return this.publishersService.findOne(id)
+  }
+}
--- src/publishers/publishers.service.ts
+++ src/publishers/publishers.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import Publisher from './publisher.entity';
 
 @Injectable()
-export class PublishersService {}
+export class PublishersService {
+  constructor(@InjectRepository(Publisher) private publisherRepository: Repository<Publisher>) {}
+  
+  findOne(id:number){
+    return this.publisherRepository.findOne(id);
+  }
+  
+  find(){
+    return this.publisherRepository.find();
+  }
+  
+  create(publisher:Publisher){
+    return this.publisherRepository.create(publisher);
+  }
+}
