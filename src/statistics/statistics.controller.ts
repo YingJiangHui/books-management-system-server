c38e51f926b59633819d0b846f596b9b64af7608
Jh <473380917@qq.com>
2021-08-03 14:36:14 +0800

    feat: 日期统计表

--- src/statistics/statistics.controller.ts
+++ src/statistics/statistics.controller.ts
@@ -1,16 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
-import { StatisticsService } from './statistics.service';
+import { Controller,Get,Query } from '@nestjs/common';
+import { StatisticsService,TimeQuantum,TimeUnit } from './statistics.service';
 
 @Controller('statistics')
 export class StatisticsController {
   constructor(private statisticsService : StatisticsService) {}
-  @Get()
+  @Get("gather")
   async getStatistics(){
     return {
       category: await this.statisticsService.getTopTenCategory(),
       book: await this.statisticsService.getTopTenBorrowedBook(),
       status: await this.statisticsService.getStatusStatistics(),
-      reader: await this.statisticsService.getTopTenActiveReader()
+      reader: await this.statisticsService.getTopTenActiveReader(),
     }
+  }
+  
+  @Get('time')
+  async getStatisticsByTime(@Query('timeUnit') timeUnit:TimeUnit,@Query('timeQuantum') timeQuantum:TimeQuantum){
+    return this.statisticsService.getStatisticsByTime(timeUnit,timeQuantum)
   }
 }
--- src/statistics/statistics.service.ts
+++ src/statistics/statistics.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Connection } from 'typeorm';
-import { start } from 'repl';
-
+export type TimeUnit = 'month'|'day'
+export type TimeQuantum = [string,string]
 @Injectable()
 export class StatisticsService {
   constructor(private readonly connection: Connection) {
@@ -20,16 +20,17 @@ export class StatisticsService {
   }
   
   async getTopTenActiveReader() {
-    return this.connection.query(`SELECT u."username","userId" ,count(*) as n FROM borrow_book, public.user as u where "userId" = u.id GROUP BY "userId",u."username" ORDER BY n DESC LIMIT 10`);
+    return this.connection.query(`SELECT u."username" as name,"userId" ,count(*) as n FROM borrow_book, public.user as u where "userId" = u.id GROUP BY "userId",u."username" ORDER BY n DESC LIMIT 10`);
   }
   
-  async getWeek() {
-    return this.connection.query(`select
-      "startedDate"::DATE-(extract(dow from "startedDate"::TIMESTAMP)-1||'day')::interval monday,
-      count(*) amount
-      from borrow_book
-      where 1=1
-      GROUP BY "startedDate"::DATE-(extract(dow from "startedDate"::TIMESTAMP)-1||'day')::interval`
-    );
+  async getStatisticsByTime(timeUnit:TimeUnit,timeQuantum:TimeQuantum=['2021-01-01','2022-01-01']) {
+    return this.connection.query(`SELECT "status",EXTRACT(${timeUnit} FROM "startedDate") as time,count(*) FROM borrow_book where "startedDate" between '${timeQuantum[0]}' and '${timeQuantum[1]}' GROUP BY time,status`)
+    // return this.connection.query(`select
+    //   "startedDate"::DATE-(extract(dow from "startedDate"::TIMESTAMP)-1||'day')::interval monday,
+    //   count(*) amount
+    //   from borrow_book
+    //   where 1=1
+    //   GROUP BY "startedDate"::DATE-(extract(dow from "startedDate"::TIMESTAMP)-1||'day')::interval`
+    // );
   }
 }
